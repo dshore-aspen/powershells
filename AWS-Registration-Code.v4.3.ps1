9b41899aada36a822cd042845f1f72eaecca59fb
dshore-aspen <63071025+dshore-aspen@users.noreply.github.com>
2022-01-18 12:18:14 -0500

    Update AWS and Forticlient
    Created v4.3 of AWS registration script to include proper exiting for devices that don't have the app installed.
    
    Updated results of if-check on Forticlient to report back exit codes instead of return codes.

---
+++ AWS-Registration-Code.v4.3.ps1
@@ -0,0 +1,31 @@
+﻿$user=$env:USERNAME
+if (! (Test-Path -Path "C:\Program Files (x86)\Amazon Web Services, Inc\Amazon WorkSpaces\workspaces.exe") ) {
+    Write-Host "[APP-CHECK] -- Application not found."
+    exit
+    }
+
+
+if (! (Test-Path -Path "C:\Users\$user.aspen_inst\appdata\Local\Amazon Web Services\Amazon WorkSpaces\") ) {
+    Write-Host "[CODE-CHECK] -- Registration file not present. Loading AWS to create registration file."
+    Start-Process -WindowStyle Minimized -FilePath "C:\Program Files (x86)\Amazon Web Services, Inc\Amazon WorkSpaces\workspaces.exe"
+    Start-Sleep -Seconds 10
+    Stop-Process -Name workspaces -Force
+    Write-Host "[CODE-CHECK] -- Workspaces has loaded and been closed."
+    } else {
+        if ( (Get-Content "C:\Users\$user.aspen_inst\appdata\Local\Amazon Web Services\Amazon WorkSpaces\RegistrationList.json" | %{$_ -like "[INSERT CODE HERE"} ) ) {
+            Write-Host "[CODE-CHECK] -- Registration code already present."
+            exit
+        }
+    }
+
+cd "C:\Users\$user.aspen_inst\appdata\Local\Amazon Web Services\Amazon WorkSpaces\"
+Set-Content ".\RegistrationList.json" '[{"registrationCode":"[INSERT CODE HERE"}]'
+Write-Host "[POST-WRITE] -- Registration code added."
+
+if ( Get-Content ".\RegistrationList.json"   ) {
+    Write-Host "[POST-CHECK] -- Registration code now present."
+    } else {
+    Write-Error -Message "[POST-CHECK] -- Missing registration code still."
+}
+
+
--- WIN-DetectFortiClientVersion.ps1
+++ WIN-DetectFortiClientVersion.ps1
@@ -1,7 +1,8 @@
 
 $forticlientVersion=$(Get-WmiObject -Class Win32_Product | where vendor -eq "Fortinet Technologies Inc" | select Version)
 IF ($forticlientVersion -match "6.4.6.1658"){
-    "Version: "$forticlientVersion ; return 0
+    Write-Host "Succeed - Version: "$forticlientVersion
+    exit 0
     }else{
-    return
+    exit 1
     }
