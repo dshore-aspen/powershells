e2fb5803a9c76ddd3c6d57c29108cc87206c83b5
dshore-aspen <derek.shore@aspeninstitute.org>
2022-03-24 11:19:07 -0400

    Updated Check Cycle
    Set the check function to check each printer domain separately instead of elseif.
    Updated log header/footer to make it clearer when the check began.
    Reordered the printer array for legibility.
    Updated date format for Year/Month/Day instead of Day/Month/Year

--- removeOldPrinters-v4.ps1
+++ removeOldPrinters-v4.ps1
@@ -1,15 +1,16 @@
-﻿$Logfile = "C:\Users\Public\Downloads\$(gc env:computername)-PrinterRemove-$(Get-Date -Format "dd.MM.yyyy").log"
+﻿$Logfile = "C:\Users\Public\Downloads\$(gc env:computername)-PrinterRemove-$(Get-Date -Format "yyyy.MM.dd").log"
 
 Function LogWrite
 {
    Param ([string]$logstring)
-   $logstring = "$(Get-Date -Format "dd.MM.yyyy HH:mm:ss") -- $logstring"
+   $logstring = "$(Get-Date -Format "yyyy.MM.dd HH:mm:ss") -- $logstring"
    Add-content $Logfile -value $logstring
 }
 
-$printerList = "Aspen-Copiers-SecurePrint", "ASPEN-Copiers-SecurePrintNew", "ASPEN-Printers-SecurePrint", "DC_Printers", "DC_Copiers", "Aspen-Copiers-SecurePrint", "ASPEN-Printers-SecurePrintNew"
+$printerList = "Aspen-Copiers-SecurePrint", "ASPEN-Copiers-SecurePrintNew", "DC_Copiers", "ASPEN-Printers-SecurePrint", "ASPEN-Printers-SecurePrintNew", "DC_Printers"
 
-LogWrite "XXXXXXXXXXXXXXXXXXXX --- Printer check starting"
+LogWrite ""
+LogWrite "XXXXXXXXXXXXXXXXXXXX --- Printer check starting --- XXXXXXXXXXXXXXXXXXXX"
 
 foreach ($printer in $printerList) {
     LogWrite "Checking for $printer"
@@ -17,12 +18,16 @@ foreach ($printer in $printerList) {
     if (Test-Path $("HKCU:\Printers\Connections\,,dcuniflow,$printer")) {
         LogWrite "$printer present under \\dcuniflow. Removing now"
         Remove-Printer -Name "\\dcuniflow\$printer"
-    } elseif (Test-Path $("HKCU:\Printers\Connections\,,dcuniflow.aspeninst.org,$printer")){
+    } else {
+        LogWrite "$printer not present on \\dcuniflow."
+    }
+    
+    if (Test-Path $("HKCU:\Printers\Connections\,,dcuniflow.aspeninst.org,$printer")){
         LogWrite "XXX -- $printer present under \\dcuniflow.aspeninst.org. Removing now"
         Remove-Printer -Name "\\dcuniflow.aspeninst.org\$printer"
     } else {
-        LogWrite "$printer not present."
+        LogWrite "$printer not present on \\dcuniflow.aspeninst.org"
     }
 }
 
-LogWrite "XXXXXXXXXXXXXXXXXXXX --- Printer check done"
+LogWrite "XXXXXXXXXXXXXXXXXXXX --- Printer check done --- XXXXXXXXXXXXXXXXXXXX"
