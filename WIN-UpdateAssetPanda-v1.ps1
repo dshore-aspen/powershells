e963a1dfa519c18600825cad3244ee339d92d82b
dshore-aspen <derek.shore@aspeninstitute.org>
2022-04-14 09:40:00 -0400

    Added WIN-UpdateAssetPanda-V1
    Script to run our "assign to user" action.
    Redacted the bearer token before uploading for security purposes.

---
+++ WIN-UpdateAssetPanda-v1.ps1
@@ -0,0 +1,39 @@
+﻿$serviceTag=$(Get-WmiObject win32_computersystemproduct).IdentifyingNumber
+$userName=$env:USERNAME
+
+
+Function API_CALL_V3
+{
+   Param ([string]$call, [string]$method)
+   $api = "https://api.assetpanda.com/v3/" + $call
+   Invoke-RestMethod $api -Method $method -Headers $headers -Body $body
+}
+
+Function API_CALL_V2
+{
+   Param ([string]$call, [string]$method)
+   $api = "https://api.assetpanda.com:443/v2/" + $call
+   Invoke-RestMethod $api -Method $method -Headers $headers -Body $body
+}
+
+
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Authorization", "Bearer [INSERT TOKEN HERE]")
+$headers.Add("Content-Type", "application/json")
+$apiV3_URI="https://api.assetpanda.com/v3"
+$body = "{
+`n    `"field_filters`": {
+`n        `"field_1`": `"GLBT3Z2`"
+`n    }
+`n}"
+
+$response = API_CALL_V3 '/groups/82676/search/objects' 'POST'
+# $response = Invoke-RestMethod $apiV3_URI'/groups/82676/search/objects' -Method 'POST' -Headers $headers -Body $body # | ConvertTo-Json
+$entity_object_id=$response.objects.id
+
+$body = "{
+        `"embedded_into_object_id`": `"`",
+        `"action_fields`": {
+          `"field_1`": "$userName"
+        }
+}"
