0a59bffcd85f7216d0498cb6f006153349bc4710
dshore-aspen <63071025+dshore-aspen@users.noreply.github.com>
2023-04-10 11:27:28 -0400

    Create WIN-DisablePowerSavingEthernet.v2.ps1

---
+++ WIN-DisablePowerSavingEthernet.v2.ps1
@@ -0,0 +1,10 @@
+﻿try {
+Set-NetAdapterAdvancedProperty -DisplayName "Energy-Efficient Ethernet" -DisplayValue "Disabled" -ErrorAction SilentlyContinue
+Set-NetAdapterAdvancedProperty -DisplayName "Green Ethernet" -DisplayValue "Disabled" -ErrorAction SilentlyContinue
+Set-NetAdapterAdvancedProperty -DisplayName "Idle Power Saving" -DisplayValue "Disabled" -ErrorAction Stop
+Write-Output "Configuration worked"
+exit 0
+} catch {
+Write-Output "Configuration failed"
+exit 1
+}
