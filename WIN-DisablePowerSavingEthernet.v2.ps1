04db589c1567c12ce2a1b59faa351bd2446d6653
dshore-aspen <derek.shore@aspeninstitute.org>
2023-04-14 12:47:38 -0400

    Update WIN-DisablePowerSavingEthernet.v2.ps1
    Added script scheduler with logic to disable script scheduler if no longer needed.

--- WIN-DisablePowerSavingEthernet.v2.ps1
+++ WIN-DisablePowerSavingEthernet.v2.ps1
@@ -3,49 +3,64 @@
 # Then it check if the settings are available to manipulate.
 # Then it updates the settings and updates RegEdit so the next time it runs it can quickly cancel out.
 
+# Variables for the RegItem check
 $RegistryPath = 'HKCU:\Environment\GreenEthernetStatus\'
 $Name = 'Status'
 $Value = '0'
 
+
+# Variables for the Script Schedule
+$ScriptLocation = $MyInvocation.MyCommand.Path
+$action = New-ScheduledTaskAction -Execute 'powershell.exe' -argument $ScriptLocation 
+$trigger = New-ScheduledTaskTrigger -AtLogOn
+$schedulSettings = New-ScheduledTaskSettingsSet -RestartCount 6 -RestartInterval (New-TimeSpan -Minutes 15) -ExecutionTimeLimit (New-TimeSpan -Seconds 30) # -DeleteExpiredTaskAfter (New-TimeSpan -Days 30)
+$ST = New-ScheduledTask -Action $action -Trigger $trigger -Settings $schedulSettings
+Register-ScheduledTask UpdateGreenEthernet01 -InputObject $ST -ErrorAction SilentlyContinue
+
+
+# Variables for the actual settings
 $AdapterList = "Energy-Efficient Ethernet","Green Ethernet","Idle Power Saving"
 $AdapterTest = Get-NetAdapterAdvancedProperty -DisplayName $AdapterList | Where-Object -FilterScript { $_.DisplayValue -eq "Enabled" }
 
 
-
-If ( -NOT (Test-Path $RegistryPath)) {
-    Write-Output "RegItem not found. Doing config."
-    if  ( $AdapterTest::IsNullOrEmpty)
-    {
-        Write-Output "Configs already in place. Exiting script"
-        break
-        exit 0
-    } else {
-        Write-Output "At least one setting not configured, updating now"
-        Set-NetAdapterAdvancedProperty -DisplayName $AdapterList -DisplayValue "Disabled" -ErrorAction SilentlyContinue
-        if ((Get-NetAdapterAdvancedProperty -DisplayName $AdapterList | Where-Object -FilterScript { $_.DisplayValue -eq "Enabled" })::IsNullOrEmpty){
-            Write-Output "Setting update did not work. Exiting script with error"
-            break
-            exit 1
-            } else {
-            Write-Output "Settings have been updated. Updating Registry."
-            try {
-                New-Item -Path $RegistryPath
-                New-ItemProperty -Path $RegistryPath -Name $Name -Value $Value -PropertyType DWORD
-                Write-Output "Registry path entry updated. Exiting."
-                break
-                exit 0
-                } catch {
-                Write-Output "Registry path entry update failed. Exiting with error."
-                break
-                exit 1
-                }
-
-            }
-
-    }
- 
-} else {
-    Write-Output "RegItem found. Configuration already complete. Self destructing to prevent future runs."
+# Check if the RegItem is already present. If yes=exit and delete schedule, if no=continue
+If ( (Test-Path $RegistryPath)) {
+    Write-Output "RegItem found. Configuration already complete. Disabling script scheduler."
+    Unregister-ScheduledTask -TaskName UpdateGreenEthernet01 -Confirm:$false
     break
     exit 0
+} 
+
+Write-Output "RegItem not found. Checking configs."
+
+# Check if any of the adapter settings are enabled.
+if  ($AdapterTest)
+{
+    Write-Output "At least one setting not configured, updating now"
+    Set-NetAdapterAdvancedProperty -DisplayName $AdapterList -DisplayValue "Disabled" -ErrorAction SilentlyContinue
+    if ((Get-NetAdapterAdvancedProperty -DisplayName $AdapterList | Where-Object -FilterScript { $_.DisplayValue -eq "Enabled" })::IsNullOrEmpty){
+        Write-Output "Setting update did not work. Exiting script with error and will try again next time."
+        break
+        exit 1
+    }
+}
+
+
+Write-Output "Configs in place. Disabling script schedule and updating RegItem."
+
+# All checks passed, writing the RegItem. If that succeeds, deleting script schedule.
+try {
+    New-Item -Path $RegistryPath
+    New-ItemProperty -Path $RegistryPath -Name $Name -Value $Value -PropertyType DWORD
+    Write-Output "Registry path entry updated. Disabling script schedule."           
+    Unregister-ScheduledTask -TaskName UpdateGreenEthernet01 -Confirm:$false
+    break
+    exit 0
+
+} catch {
+
+# Unable to create the RegItem, leaving schedule intact.
+    Write-Output "Registry path entry update failed. Leaving script schedule intact. Exiting with error."
+    break
+    exit 1
 }
